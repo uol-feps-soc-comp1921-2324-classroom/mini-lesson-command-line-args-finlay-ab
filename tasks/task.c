f9611648d0514d65cad896a3e1ef210c1df5165b
finlay brydges <100164471+finlay-ab@users.noreply.github.com>
2024-02-05 14:55:04 +0000

    completed tasks.

--- tasks/task.c
+++ tasks/task.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h> 
+#include <stdio.h> 
 
-int main() {
-    int num1, num2;
+int main(int argc, char *argv[])
+{
+    printf("%d\n", argc);
+    if (argc != 3)
+    {
+        printf("Usage: %s <number1> <number2>\n", argv[0]);
+        return 0;
+    }
 
-    // Prompt the user for input
-    printf("Enter the first number: ");
-    scanf("%d", &num1);
-
-    printf("Enter the second number: ");
-    scanf("%d", &num2);
+    if (isdigit(argv[1]) && isdigit(argv[2]))
+    {
+        printf("both inputs must be digits!");
+        return 1;
+    }
 
     // Calculate and print the sum
-    int sum = num1 + num2;
+    int sum = atoi(argv[1]) + atoi(argv[2]);
     printf("Sum: %d\n", sum);
 
     return 0; // Exit successfully
